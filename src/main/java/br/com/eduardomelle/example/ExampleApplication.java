b61479ee930ff55e0a1940d29cb8f53cb1c9292a
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2024-01-02 20:10:18 -0300

    first commit

---
+++ src/main/java/br/com/eduardomelle/example/ExampleApplication.java
@@ -0,0 +1,27 @@
+package br.com.eduardomelle.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/hello")
+class TestController {
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+}
